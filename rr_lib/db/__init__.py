31a34d822a285bfe23cc7c9eabb3eb7a4ac64d03
Proskuryakov Alesha <a.proskuryakov@propellerads.net>
2016-10-20 03:58:44 +0300

    fix db connection

--- rr_lib/db/__init__.py
+++ rr_lib/db/__init__.py
@@ -9,11 +9,11 @@ log = logging.getLogger("DB")
 class DBHandler(object):
     def __init__(self, name="?", uri=None, db_name=None):
         cm = ConfigManager()
-        uri = uri or cm.get("mongo_uri")
-        db_name = db_name or cm.get("db_name")
+        _uri = uri or cm.get("mongo_uri")
+        _db_name = db_name or cm.get("db_name")
 
-        self.client = MongoClient(host=uri, maxPoolSize=10, connect=False)
-        self.db = self.client[db_name]
+        self.client = MongoClient(host=_uri, maxPoolSize=10, connect=False)
+        self.db = self.client[_db_name]
         self.collection_names = self.db.collection_names(include_system_collections=False)
 
-        log.info("Start DBHandler for [%s] to [%s/%s]" % (name, uri, db_name))
+        log.info("Start DBHandler for [%s] to [%s/%s]" % (name, _uri, _db_name))
