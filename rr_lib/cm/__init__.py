10f52cca34bf1bd4bd6f54ec5787f1f63ebbb01d
alesha <alexey.proskuryakov@gmail.com>
2017-10-24 00:33:43 +0300

    fix config manager groups

--- rr_lib/cm/__init__.py
+++ rr_lib/cm/__init__.py
@@ -22,7 +22,7 @@ class Singleton(type):
     _instances = {}
 
     def __call__(cls, *args, **kwargs):
-        key = (cls, kwargs.get("group"))
+        key = (cls, hash("%s%s"%(args,kwargs)))
         saved = cls._instances.get(key)
         if not saved:
             cls._instances[key] = super(Singleton, cls).__call__(*args, **kwargs)
@@ -37,15 +37,16 @@ class ConfigManager(object):
             config_file = "%s/config_test.json" % module_path()
         else:
             config_file = os.path.join(os.environ.get("OPENSHIFT_DATA_DIR", ""), os.environ.get("config_file", ""))
-        config_file = config_f or config_file
+        config_file = config_f or config_file or 'config_test.json'
         try:
             f = open(config_file, )
+            raw_data = '\n'.join(f.readlines())
         except Exception as e:
             log.exception(e)
             log.error("Can not read config file %s" % config_file)
             sys.exit(-1)
 
-        self.config_data = json.load(f)
+        self.config_data = json.loads(raw_data)
         self.config_file = config_file
 
         log.info(
@@ -65,3 +66,6 @@ class ConfigManager(object):
 if __name__ == '__main__':
     assert ConfigManager(group=1) != ConfigManager()
     assert ConfigManager(group=1) == ConfigManager(group=1)
+
+    cm = ConfigManager(config_f='/home/alesha/Dropbox/rr/config_generators.json', group=2)
+    print cm.get('sniffer')
