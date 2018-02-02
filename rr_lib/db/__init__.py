910c3962e70348d50ef189416c48f9079de49bed
Alesha <alexey.proskuryakov@gmail.com>
2018-02-02 16:43:23 +0300

    use auth at db

--- rr_lib/db/__init__.py
+++ rr_lib/db/__init__.py
@@ -10,16 +10,23 @@ log = logging.getLogger("DB")
 class DBHandler(object):
     __metaclass__ = Singleton
 
-    def __init__(self, name="main", uri=None, db_name=None, connection_name=''):
+    def __init__(self, name="main", connection_name=''):
         cm = ConfigManager()
-        if not cm.get(name):
+        _db_config = cm.get(name, ).get('mongo', )
+        if not cm.get(name) or not _db_config:
             raise ValueError("No %s config for connect to mongo:(" % name)
 
-        _uri = uri or cm.get(name, ).get('mongo', ).get("uri", )
-        _db_name = db_name or cm.get(name, ).get('mongo', ).get("db_name", )
+        _uri = _db_config.get("uri", )
+        _db_name = _db_config.get("db_name", )
+        _u = _db_config.get('user')
+        _p = _db_config.get('pwd')
+        conn_params = {'host': _uri, 'maxPoolSize': 200, 'connect': False, 'connectTimeoutMS': 5000}
+        if _u and _p:
+            conn_params['username'] = _u
+            conn_params['password'] = _p
 
-        self.client = MongoClient(host=_uri, maxPoolSize=200, connect=False, connectTimeoutMS=5000)
+        self.client = MongoClient(**conn_params)
         self.db = self.client[_db_name]
         self.collection_names = self.db.collection_names(include_system_collections=False)
 
-        log.info("Start DBHandler for [%s] to [%s/%s]" % (connection_name or '-', _uri, _db_name))
+        log.info("Start DBHandler for [%s] to [%s@%s/%s]" % (connection_name or '-', _u, _uri, _db_name))
