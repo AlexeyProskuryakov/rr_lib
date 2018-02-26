38d71670d9a8ee36d224a0e1a4d1b29a010136b5
Alesha <alexey.proskuryakov@gmail.com>
2018-02-26 20:07:07 +0300

    fucking configs

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='rr_lib',
-      version='0.0.8',
+      version='0.0.9',
       description='my lib for using in rr project',
       author='alexeyka',
       author_email='stack2008@gmail.com'
