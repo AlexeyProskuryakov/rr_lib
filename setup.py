8ca0f9c74ddf4621bfed947e2c5ba56a0788f23f
Alesha <alexey.proskuryakov@gmail.com>
2018-06-13 23:58:45 +0300

    py3 fix

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='rr_lib',
-      version='0.1.0',
+      version='0.1.1',
       description='my lib for using in rr project',
       author='alexeyka',
       author_email='stack2008@gmail.com'
